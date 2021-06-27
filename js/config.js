eb58f85cc14da73fb22ba0687b4b7c6a67050ec2
Garrettgar21 <56242627+Garrettgar21@users.noreply.github.com>
2021-06-27 19:09:05 -0400

    This is the final file upload

---
+++ js/config.js
@@ -0,0 +1,10 @@
+window._config = {
+    cognito: {
+        userPoolId: 'us-east-1_p513RNFH4',
+        userPoolClientId: '5f2v0niivns5m8nc8rqb3kc77a', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-east-1' // e.g. us-east-2
+    },
+    api: {
+        invokeUrl: 'https://p8sdx8ifvl.execute-api.us-east-1.amazonaws.com/prod' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+    }
+};
